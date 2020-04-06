6c493736c9bd5411c68db03ad8ef1add1e64b1a5
taroj khan <tarujkhan@yahoo.com>
2020-04-06 22:41:53 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -72,9 +72,9 @@ end
     # name = new_dog[1]
     # breed = new_dog[2]
     # new_dog = self.new(id:id, name:name, breed:breed)
-    def self.find_by_id(id)
+    def self.find_by_name(name)
     sql = <<-SQL
-    SELECT * FROM dogs WHERE id = ? LIMIT 1
+    SELECT * FROM dogs WHERE name = ? LIMIT 1
     SQL
     DB[:conn].execute(sql,id).map do |row|
       self.new_from_db(row)
