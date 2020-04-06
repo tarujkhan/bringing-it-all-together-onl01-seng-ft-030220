7208b65df712165903865970ffc8bcb488bdf960
taroj khan <tarujkhan@yahoo.com>
2020-04-06 22:41:57 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -76,7 +76,7 @@ end
     sql = <<-SQL
     SELECT * FROM dogs WHERE name = ? LIMIT 1
     SQL
-    DB[:conn].execute(sql,id).map do |row|
+    DB[:conn].execute(sql,name).map do |row|
       self.new_from_db(row)
     end.first 
    end 
