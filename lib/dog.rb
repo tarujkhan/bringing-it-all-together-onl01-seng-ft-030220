8ef277d5b63fdc9b8517c837e319bbce01725d84
taroj khan <tarujkhan@yahoo.com>
2020-04-06 21:46:46 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -20,7 +20,7 @@ class Dog
     SQL
    
     DB[:conn].execute(sql, self.name, self.breed)
-    @id = DB[:conn].execute(SELECT last_insert_rowid)
+    @id = DB[:conn].execute("SELECT last_insert_rowid(), [id])
   end 
   
   def self.drop_table
