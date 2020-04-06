3ba34f3fa41cf6c1e53fb9a97ff29d635468db6f
taroj khan <tarujkhan@yahoo.com>
2020-04-06 21:34:12 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -18,6 +18,7 @@ class Dog
     sql = <<-SQL
     INSERT INTO dogs (id, name, breed) VALUES (?, ?, ?)
     SQL
+    new_dog = self.new(id, name, breed)
     DB[:conn].execute(sql, self.id, self.name, self.breed)
   end 
   
